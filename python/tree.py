9804e2871c3a868f62b8233c104d352201304222
shafayetejaman <shafayetejaman@gmail.com>
2024-03-06 07:28:25 +0000

    Wed, Mar 6, 2024, 1:28 PM +06:00

--- python/tree.py
+++ python/tree.py
@@ -174,19 +174,20 @@ class Solution:
 
 # 404. Sum of Left Leaves
 
+
 class Solution:
     def sumOfLeftLeaves(self, root) -> int:
-        
+
         def dfs(root, parent):
             if not root:
                 return 0
-            
-            if not root.left and not root.right and root == parent.left:
+
+            if not root.left and not root.right and parent and root == parent.left:
                 return root.val
-            
-            _sum = self.sumOfLeftLeaves(root.left, root)
-            _sum += self.sumOfLeftLeaves(root.right, root)
-            
+
+            _sum = dfs(root.left, root)
+            _sum += dfs(root.right, root)
+
             return _sum
-        
+
         return dfs(root, None)
