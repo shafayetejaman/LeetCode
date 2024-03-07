ed873f7ad1f5a54c340440fc2ba3f655c278858a
shafayetejaman <shafayetejaman@gmail.com>
2024-03-07 10:18:02 +0000

    Thu, Mar 7, 2024, 4:18 PM +06:00

--- python/array.py
+++ python/array.py
@@ -44,15 +44,28 @@ class Solution:
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums = set(nums)
-        ans = [nums[0]]
-        
-        if nu
+        nums = list(nums)
+        ans = [max(nums)]
+
+      
+        if len(nums) < 3:
+            return ans[0]
+
+        ans.append(nums[1])
+        ans.append(nums[2])
 
         for i in nums:
-            if ans[0] < i:
-                ans[0] = i
-                
-        for i in nums:
-            
+            if i == ans[0]:
+                continue
+
             if ans[1] < i:
                 ans[1] = i
+
+        for i in nums:
+            if i == ans[0] or i == ans[1]:
+                continue
+
+            if ans[2] < i:
+                ans[2] = i
+
+        return ans[2]
