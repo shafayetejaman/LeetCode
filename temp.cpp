186339d9f42ceeedf3ed118ee36d1ba104936887
shafayetejaman <shafayetejaman@gmail.com>
2024-03-08 17:15:57 +0000

    Fri, Mar 8, 2024, 11:15 PM +06:00

--- temp.cpp
+++ temp.cpp
@@ -1,62 +1,53 @@
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define line
+#define print(args...)
+#endif
+
 using namespace std;
-void conquer(int arr[], int si, int mid, int ei)
+using namespace __gnu_pbds;
+
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef pair<long long, long long> pll;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+#define loop(n) for(auto j = 0; j < (n); j++)
+#define even(n) ((n) % 2 == 0)
+#define odd(n) ((n) % 2 != 0)
+#define LL (long long)
+#define LD (long double)
+#define ll long long
+#define ln '\n'
+#define all(x) (x).begin(), (x).end()
+
+// const int N = 1e5 + 1;
+// const int M = 1e9 + 7;
+
+void solve()
 {
-    vector<int> merge(ei - si + 1);
-    int idx1 = si;
-    int idx2 = ei;
-    int x = 0;
+    
 
-    while (arr[idx1] <= mid && arr[idx2] <= ei)
-    {
-        if (arr[idx1] <= arr[idx2])
-        {
-            merge[x++] = arr[idx1++];
-        }
-        else
-        {
-            merge[x++] = arr[idx2];
-        }
-        while (arr[idx1] <= mid)
-        {
-            merge[x++] = arr[idx1++];
-        }
-
-        while (arr[idx2] <= ei)
-        {
-            merge[x++] = arr[idx1++];
-        }
-        for (int i = 0, j = si; i < merge.size(); i++, j++)
-        {
-            arr[i] = merge[i];
-        }
-    }
-
-}
-void devide(int arr[], int si, int ei)
-{
-    if (si >= ei) return;
-    int mid = si + (si - ei) / 2;
-    devide(arr, si, mid);
-    devide(arr, mid + 1, ei);
-    conquer(arr, si, mid, ei);
+    // cout << ln;
 }
 
 int main()
 {
-    // int n;
-    // int arr[n] = {1, 3, 4, 5, 56, 67, 78, };
-    // devide(arr, 0, n - 1);
-    cout << "int :" << sizeof(int) << endl;
-    cout << "long : " << sizeof(long) << endl;
-    cout << "long long : " << sizeof(long long) << endl;
-    cout << "float : " << sizeof(float) << endl;
-    cout << "double : " << sizeof(double) << endl;
-    cout << "long double : " << sizeof(long double) << endl;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
+
+#ifndef LOCAL
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#endif
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) solve();
 
     return 0;
 }
