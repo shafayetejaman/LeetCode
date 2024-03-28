6a30fea208266296bda33ff7ba252241c0a2a481
shafayetejaman <shafayetejaman@gmail.com>
2024-03-28 05:11:15 +0000

    Thu, Mar 28, 2024, 11:11 AM +06:00

--- auto_form_submit.py
+++ auto_form_submit.py
@@ -2,7 +2,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-form_link = "https://docs.google.com/forms/d/e/1FAIpQLSeYE5oKTpILggujs0EhdI765ejmhKxECFKYwU6emK2_cwBa7g/viewform"
+form_link = "https://docs.google.com/forms/d/e/1FAIpQLSeZLgEKjFpRhDlEGJ_A_oHMjx_iawd0gCiuFB4f7CTMa7H9cg/viewform"
 # form_link = "https://docs.google.com/forms/d/e/1FAIpQLScSZzLetdC7yvCmpkmn7fVEcMdhSkmpXIGdMTqVcuRmlgnrwQ/formrestricted"
 
 
