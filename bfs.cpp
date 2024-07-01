c9d0f6619f919e45dc85499d63c722a75d558dc1
shafayetejaman <shafayetejaman@gmail.com>
2024-07-01 11:18:35 +0000

    Mon, Jul 1, 2024, 5:18 PM +06:00

--- bfs.cpp
+++ bfs.cpp
@@ -1,89 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e3 + 5;
+const int N = 1e5 + 5;
 vector<int>adjlist[N];
 bool visited[N];
 int level[N];
-
-void bfs(int s, bool print)
+void bfs(int s)
 {
+    visited[s] = true;
     queue<int>q;
     q.push(s);
-    visited[s] = true;
     level[s] = 0;
-    //cout<<"visited S ="<<s<<endl;
     while (!q.empty())
     {
-        int parent = q.front();
+        int p = q.front();
         q.pop();
-
-        // print the nodes of the components
-        if (print) cout << parent << " ";
-
-        for (int child : adjlist[parent])
+        for (int c = 0; c < adjlist[p].size();c++)
         {
-            if (visited[child]) continue;
+            int child = adjlist[p][c];
+            if (visited[child])continue;
             q.push(child);
             visited[child] = true;
-            level[child] = level[parent] + 1;
-        
-            
-
+            level[child] = level[p] + 1;
         }
     }
-
 }
 int main()
 {
+    for (int i = 0; i < N; i++)
+    {
+        level[i] = -1;
+    }
     int n, m;
     cin >> n >> m;
-    for (int i = 1; i <= m; i++)
+    for (int i = 0; i < m; i++)
     {
         int u, v;
         cin >> u >> v;
         adjlist[u].push_back(v);
         adjlist[v].push_back(u);
-
-
     }
-
-
-    // code for connect components :
-
-    // count the number of components
-    int cc = 0;
-
-    for (int i = 1; i <= n; i++)
+    bfs(0);
+    int target;
+    cin >> target;
+    vector<int>kyl;
+    for (int i = 0; i < n; i++)
     {
-        if (visited[i]) continue;
-
-        cc++;
-        bfs(i, false);
-
+        if (level[i] == target)
+        {
+            kyl.push_back(i);
+        }
     }
-    cout << cc << endl;
-
-    // reset
-    cc = 0;
-
-    for (auto i = 0; i < N; i++)
+    if (kyl.empty())
     {
-        visited[i] = 0;
+        cout << -1;
     }
-
-    // print the components
-
-    for (int i = 1; i <= n; i++)
+    else
     {
-        if (visited[i]) continue;
-
-        cc++;
-        cout << "Component " << cc << " : ";
-        bfs(i, true);
-        cout << endl;
+        sort(kyl.begin(), kyl.end());
+        for (int i = 0; i < kyl.size(); i++)
+        {
+            cout << kyl[i] << " ";
+        }
     }
-
-    
-
     return 0;
 }
