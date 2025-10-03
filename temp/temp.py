104da0f5dc1d28d71749e751177791d69b086fd3
shafayetejaman <shafaitzaman@gmail.com>
2025-10-03 18:13:03 +0600

    add .gitignore, update VSCode settings, and implement solution for generating valid parentheses

--- temp/temp.py
+++ temp/temp.py
@@ -1,3 +1,30 @@
-nums = [1,2,3,4]
-cnt = [1 for x in nums if x % 2 == 0]
-print(cnt)
+class Solution:
+    def isValid(self, s: str):
+        st = []
+        for char in s:
+            if char == "(":
+                st.append("(")
+            elif not st:
+                return False
+            else:
+                st.pop()
+        return len(st) == 0
+
+    def generateParenthesis(self, n: int) -> list[str]:
+        ans: list[str] = []
+
+        def dfs(s):
+            nonlocal ans
+            if len(s) == 2 * n:
+                if self.isValid(s):
+                    ans.append(s)
+
+                return
+
+            dfs(s + "(")
+            dfs(s + ")")
+
+        dfs("")
+
+        return ans
+
